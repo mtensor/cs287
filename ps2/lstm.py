5b2ecf70be0acb8b7be2321018b0443d9e4e10b5
Maxwell Nye <mnye@mit.edu>
2019-02-20 18:00:58 -0500

    lstm

--- ps2/lstm.py
+++ ps2/lstm.py
@@ -11,6 +11,7 @@ import numpy as np
 from torchtext.data.iterator import BPTTIterator
 from torchtext.data import Batch, Dataset
 import math
+import time
 
 
 # Our input $x$
@@ -65,40 +66,44 @@ class NamedBpttIterator(BPTTIterator):
 train_iter, val_iter, test_iter = NamedBpttIterator.splits(
     (train, val, test), batch_size=10, device=torch.device("cuda"), bptt_len=32, repeat=False)
 
-LSTMmodel(nn.Module):
-    def __init__(self, embedding_size=128, hidden_size=512, num_layers=1, vocab_size=1001, use_pretrained=False):
+class LSTMmodel(nn.Module):
+    def __init__(self, embedding_size=128, hidden_size=512, num_layers=1, vocab_size=1002, use_pretrained=False):
         super(LSTMmodel, self).__init__()
 
         
         self.embedding = ntorch.nn.Embedding(
                     vocab_size, embedding_size
-                )#.augment("h")
+                )
         if use_pretrained:
+            assert False, "need pretrained embeddings"
             self.embedding.weight.data.copy_(pretrained_embeddings)
             self.embedding.weight.requires_grad = mode == "nonstatic"
         
         #decoder, outputs to "vocab"
 
         #(seq_len, batch_size, features)
-        self.lstm = nn.LSTM(embedding_size,
+        self.lstm = ntorch.nn.LSTM(embedding_size,
                             hidden_size=hidden_size,
                             num_layers=num_layers,
                             batch_first=False,
                             dropout = 0.3
-                            ).spec("input", "seqlen", "output")
+                            ).spec("embedding", "seqlen", "output")
 
         self.decoder = ntorch.nn.Linear(
-            num_filters * len(kernel_sizes), vocab_size
-        ).spec("hidden_size", "vocab")
+            hidden_size, vocab_size
+        ).spec("output", "vocab")
+
         self.lossfn = ntorch.nn.CrossEntropyLoss().spec("vocab") 
 
     def forward(self, batch):
+        #print("batch:", batch)
+        batch = self.embedding(batch)
 
         out, _ = self.lstm(batch)
 
         dist = self.decoder(out)
         #out is a dist over vocab for the batches, log likelihood space 
-
+        return dist
 
     def loss(self, batch):
         prediction = self(batch.text)  # probabilities
@@ -106,17 +111,42 @@ LSTMmodel(nn.Module):
 
 
 def test_code(model):
-    assert False
+    #TODO .. fix test code
+    with open("sample.txt", "w") as fout:
+        print("id,word", file=fout)
+        for i, l in enumerate(open("input.txt"), 1):
+            #w_2, w_1 = l.split(' ')[-3: -1]
+            batch = [TEXT.vocab.stoi[word] for word in l.split(' ')]
+            batch = torch.tensor(batch).unsqueeze(1)
+            batch = ntorch.tensor(batch, names=("seqlen", "batch")).cuda()
+            #prediction_dict = Counter()  
+            prediction_dist = model(batch)
+
+            #prediction_dict[TEXT.vocab.stoi["<eos>"]] = 0
+            #prediction_dict[TEXT.vocab.stoi["<unk>"]] = 0
+            #prediction_dict[TEXT.vocab.stoi["<pad>"]] = 0
+            
+            prediction_dist = prediction_dist.get("seqlen", prediction_dist.shape['seqlen'] -1)
+            #predictions = [TEXT.vocab.itos[i] for i in prediction_dist.topk("vocab", 20)[1] ]
+
+            top20 = prediction_dist.topk("vocab", 20)[1]
+
+            predictions = [TEXT.vocab.itos[top20.get("vocab",i).item()] for i in range(20)]
+            #print("predictions", predictions)
+
+            print("%d,%s"%(i, " ".join(predictions)), file=fout)
 
 if __name__ == "__main__":
 
-    model = LSTMmodel(pretrained_embeddings=pretrained_embeddings) #TODO
+    debug = False
+    model = LSTMmodel() #TODO
     model.cuda()
+    #model.cpu()
 
     parameters = filter(lambda p: p.requires_grad, model.parameters())
     optimizer = torch.optim.Adam(parameters, lr=0.001)
 
-    for epoch in range(20):
+    for epoch in range(5 if not debug else 1):
         tic = time.time()
         # eval_acc, sentence_vector = evaluate(model, x_test, y_test)
         model.train()
@@ -128,7 +158,16 @@ if __name__ == "__main__":
             optimizer.step()
             losses.append(loss.item())
 
+            if i%200==0:
+                print(
+                "\tavg train_loss: {:.3f} ({:.1f}s)".format(
+                    sum(losses[-200:])/len(losses[-200:]), time.time() - tic
+                )
+            )
+                if debug: break
+
         model.eval()
+        print("computing val loss ...")
         eval_ll = 0
         for i, batch in enumerate(val_iter):
             new_eval_ll = model.loss(batch)
